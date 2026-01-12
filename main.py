8573c5f2e49b62f4ddc73d5ac8fcbbf2e3605dd9
MintBloom <a.stegmann09@gmail.com>
2026-01-12 13:26:31 +0000

    added a timer function

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 # this dashboard will call from OpenWeatherMap (OWM)
-import requests
+import requests, time
 
 baseURL_lat_lon = "https://api.openweathermap.org/data/2.5/forecast?lat=57.14369&lon=-2.09814&appid=036478abe20af19a7e11a444ce1d5eff&units=metric"
 # url needed if specify location with latitude and longitude coordinates
 baseURL_city_name = "https://api.openweathermap.org/data/2.5/forecast?q=Aberdeen,gb&mode=json&appid=036478abe20af19a7e11a444ce1d5eff&units=metric"
 # url needed if specify location with city name and country code
 # error when using city_name url was because of "mode=xml" instead of "mode=json"
+
+def timer(retrieval_func):
+    # supposed to be a decorator which times how long a function takes to execute
+    start_time = time.time()
+    retrieval_func
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print (elapsed_time)
+
 
 def retrieve_weather_info():
     # sends request to api, with a url, and recieves the data returned
@@ -21,7 +30,7 @@ def retrieve_weather_info():
 weather_info = retrieve_weather_info()
 
 if weather_info:
-    print(f"{weather_info["list.weather.main"]}") # not recognised
+    print(weather_info) # not recognised
 
 
     
